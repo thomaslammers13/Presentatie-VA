85b784402c0dc6f12e1962014d07d79c3bc1799f
thomaslammers13 <114410453+thomaslammers13@users.noreply.github.com>
2022-11-09 21:57:44 +0100

    Update Streamlitcode.py

--- Streamlitcode.py
+++ Streamlitcode.py
@@ -18,10 +18,9 @@ import datetime
 import time
 #import plost
 #from shapely.geometry import Point
-#import matplotlib.pyplot as plt
-#from scipy import stats
-#import matplotlib.pyplot as plt
-#from scipy import stats
+import matplotlib.pyplot as plt
+from scipy import stats
+
 
 pages = st.sidebar.radio('Menu', options=['Home','Verbruik Bestelwagens', 'Verbruik Vrachtwagens', 'Energieverbruik', "Voorspelling elektrische auto's", "Kaart WFO", "Kaart Schiphol Trade Centre"])
 
